5c5e4d0fc4804e224ce51964281f04f0ca8f938e
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-06-21 08:34:44 +0000

    Added openAIapi key

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
-from langchain.chat_models import ChatAnthropic
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatAnthropic()
+template = """Atom Bot is a large language model trained by OpenAI.
 
-messages = [
-    HumanMessage(
-        content="Translate this sentence from English to French. I love programming."
-    )
-]
-res = chat(messages)
+Atom Bot is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Atom Bot is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
 
-ans = res[0]
-reply = ans.content
+Atom Bot is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Atom Bot is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Atom Bot is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Atom Bot is here to assist.
+
+{history}
+Human: {human_input}
+Atom Bot:"""
+
+prompt = PromptTemplate(input_variables=["history", "human_input"], template=template)
+
+
+chatgpt_chain = LLMChain(
+    llm=OpenAI(temperature=0),
+    prompt=prompt,
+    verbose=True,
+    memory=ConversationBufferWindowMemory(k=2),
+)
+
+output = chatgpt_chain.predict(
+    human_input="I want you to act as a Linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. Do not write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in English I will do so by putting text inside curly brackets {like this}. My first command is pwd."
+)
+print(output)
+
+
