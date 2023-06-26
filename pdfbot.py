1cf4cac4b34c458d552960007bc090e9b220b904
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-06-26 12:34:55 +0000

    Added pdf file

--- pdfbot.py
+++ pdfbot.py
@@ -1,8 +1,9 @@
-import streamlit as st
 from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Chroma
 from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.document_loaders import PyMuPDFLoader
@@ -15,64 +16,37 @@ load_dotenv()
 
 user_api_key = os.getenv("OPENAI_API_KEY")
 
-# user_api_key = st.sidebar.text_input(
-#     label="#### Your OpenAI API key 👇",
-#     placeholder="Paste your openAI API key, sk-",
-#     type="password")
-
-uploaded_file = st.sidebar.file_uploader("upload", type="csv")
-
 data = None
 
-loader = PyMuPDFLoader("./docs/example.pdf")
+loader = PyMuPDFLoader("./example.pdf")
 data = loader.load()
 
-vectorstore = None
+loader = PyMuPDFLoader(pdf_path)
+documents = loader.load()
 
-if data:
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_documents(data, embeddings)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=10)
+texts = text_splitter.split_documents(documents)
 
-# embeddings = OpenAIEmbeddings()
-# vectorstore = FAISS.from_documents(data, embeddings)
+embeddings = OpenAIEmbeddings()
+vectordb = Chroma.from_documents(documents=texts, 
+                                 embedding=embeddings,
+                                 persist_directory=persist_directory)
+vectordb.persist()
 
-if vectorstore:
-    chain = ConversationalRetrievalChain.from_llm(
-    llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo'),
-    retriever=vectorstore.as_retriever())
+retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+llm = ChatOpenAI(model_name='gpt-4')
 
-def conversational_chat(query):
-    
-    result = chain({"question": query, 
-    "chat_history": st.session_state['history']})
-    st.session_state['history'].append((query, result["answer"]))
-    
-    return result["answer"]
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
-if 'history' not in st.session_state:
-    st.session_state['history'] = []
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["Hello ! Ask me anything about " + uploaded_file.name + " 🤗"]
-if 'past' not in st.session_state:
-    st.session_state['past'] = ["Hey ! 👋"]
-    
-#container for the chat history
-response_container = st.container()
-#container for the user's text input
-container = st.container()
+while True:
+        user_input = input("Enter a query: ")
+        print("")
+        if user_input == "exit":
+            break
 
-with container:
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
-        submit_button = st.form_submit_button(label='Send')
-        
-    if submit_button and user_input:
-        output = conversational_chat(user_input)
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
-
-if st.session_state['generated']:
-    with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
-            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+        query = f"###Prompt {user_input}"
+        try:
+            llm_response = qa(query)
+            print(llm_response["result"])
+        except Exception as err:
+            print('Exception occurred. Please try again', str(err))
