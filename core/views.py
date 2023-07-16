150660fdac8fbf07b8ed7848fc247c500b324efa
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2023-07-16 11:01:05 +0000

    Working with htmx to show elemtns and hide

--- core/views.py
+++ core/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .forms import prompt
 
 # Create your views here.
 def index(request):
     """"""
-    
-
-
     return render(request, "index.html")
 
+
 def getdata(request):
-    """"""
     if request.method == 'POST':
         form = prompt(request.POST)
-
         if form.is_valid():
-            query = form.cleaned_data['prompt']
+            query = form.cleaned_data
             print(query)
+            return JsonResponse({'status': 'success'})  # Return a JSON response indicating success
+        else:
+            return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)  # Return a JSON response indicating errors
     else:
         form = prompt()
-
-    return {'form': form}
+    return render(request, 'index.html', {'form': form})
